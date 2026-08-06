6c923a08e96ca721de05f7a5a195587f89204fe2
1rishu0 <you@example.com>
2026-08-06 10:10:00 +0530

    commit

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import Form from "../components/Form"
+
+function Login(){
+
+    return (
+        <>
+        <Form route="/api/token/" method="login"></Form>
+        </>
+    )
+}
+
+export default Login
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+function NotFound(){
+    return (
+        <>
+        <div>
+            <h1>404 Not Found</h1>
+            <p>The Page You are Looking for Doesn't Exist!</p>
+        </div>
+        </>
+    )
+}
+
+export default NotFound
